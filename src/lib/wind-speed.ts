65211a1f0cf7716f93898cb1f09810cf81de8165
daniel0411-cell <310135160+daniel0411-cell@users.noreply.github.com>
2026-08-24 15:03:48 +0800

    chore: untrack local drafts and workspace files

--- src/lib/wind-speed.ts
+++
@@ -1,3 +0,0 @@
-export const knotsToMph = (knots: number) => knots * 1.15078;
-export const knotsToKmh = (knots: number) => knots * 1.852;
-export const formatWind = (knots: number) => `${knots} kt (${Math.round(knotsToMph(knots))} mph / ${Math.round(knotsToKmh(knots))} km/h)`;
